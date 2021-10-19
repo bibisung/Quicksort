3010af30899052dfd461b6f125c379a17deb078e
Pillsang Sung <schmidtiana95@gmail.com>
2021-10-19 16:12:47 +0900

    quicksort v1

--- quick-sort.js
+++ quick-sort.js
@@ -1,7 +1,7 @@
 export default function(array) {
     if(array.length <= 1) { return array }
     const pivot = array[0]
-    const front = array.filter(num => num < pivot)
+    const front = array.filter(num => num <= pivot)
     const back = array.filter(num => num > pivot)
     return solution(front).concat(pivot, solution(back))
 }
